7d88ec3447dd4c2262838f46345d09b370214e9f
Accercine <45529543+Accercine@users.noreply.github.com>
2019-02-10 23:45:14 +0530

    Debug php

--- mod_form.php
+++ mod_form.php
@@ -15,7 +15,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
   $event_desc = test_input($_POST["event_desc"]);
   $username = test_input($_POST["username"]);
   $auth_code = test_input($_POST["auth_code"]);
-  $event_venue = test_input($_POST["auth_code"]);
+  $event_venue = test_input($_POST["event_venue"]);
 }
 
 //
@@ -28,45 +28,47 @@ echo "$username , $auth_code <br>";
 
 $event_venue_err = $event_name_err = $start_time_err = $end_time_err =  $event_desc_error = $username_err = $auth_code_err = "";
 
-if ($_SERVER["REQUEST_METHOD"] == "POST") {
-  //event name val
-  if (empty($_POST["event_name"])) {
-    $event_name_err = "Name is required";
-  } else if (!preg_match("/^[a-zA-Z ]*$/",$event_name)) {
-  $event_name_err = "Only letters and white space allowed";
-  }
-  else {
-    $event_name = test_input($_POST["event_name"]);
-  }
-  //event time val
-  if (empty($_POST["start_time"])){
-    $start_time_err = "Time is required";
-  } else {
-    $start_time = test_input($_POST["start_time"]);
-  }
-  if (empty($_POST["end_time"])){
-    $start_time_err = "Time is required";
-  } else {
-    $end_time = test_input($_POST["end_time"]);
-  }
+// if ($_SERVER["REQUEST_METHOD"] == "POST") {
+//   //event name val
+//   if (empty($_POST["event_name"])) {
+//     $event_name_err = "Name is required";
+//   } else if (!preg_match("/^[a-zA-Z ]*$/",$event_name)) {
+//   $event_name_err = "Only letters and white space allowed";
+//   }
+//   else {
+//     $event_name = test_input($_POST["event_name"]);
+//   }
+//   //event time val
+//   if (empty($_POST["start_time"])){
+//     $start_time_err = "Time is required";
+//   } else {
+//     $start_time = test_input($_POST["start_time"]);
+//   }
+//   if (empty($_POST["end_time"])){
+//     $start_time_err = "Time is required";
+//   } else {
+//     $end_time = test_input($_POST["end_time"]);
+//   }
 
-  if(empty($_POST["auth_code"])){
-    $auth_code_err = "Please verify authentication code.";
-  } else{
-    $auth_code = test_input($_POST["auth_code"]);
-  }
-  if(empty($_{POST["event venue"]})){
-    $event_venue_err = "Please enter a venue for the event";
-  } else{
-    $event_venue = test_input($_POST["event_venue"]);
-  }
-  if(true){
-    $event_desc = test_input($_POST["event_desc"]);
-    $username = test_input($_POST["username"]);
-  }
-}
+//   if(empty($_POST["auth_code"])){
+//     $auth_code_err = "Please verify authentication code.";
+//   } else{
+//     $auth_code = test_input($_POST["auth_code"]);
+//   }
+//   if(empty($_{POST["event_venue"]})){
+//     $event_venue_err = "Please enter a venue for the event";
+//   } else{
+//     $event_venue = test_input($_POST["event_venue"]);
+//   }
+//   if(true){
+//     $event_desc = test_input($_POST["event_desc"]);
+//     $username = test_input($_POST["username"]);
+//   }
+// }
 
-$net_str = trim($username) + " "+ $auth_code;
+$net_str = trim($username);
+$net_str .= " ";
+$net_str .= $auth_code;
 $b = false;
 $auth_file = fopen("auth_code.txt", "r");
 
@@ -79,7 +81,7 @@ while(!feof($auth_file)){
   }
 }
 
-if(b==true){
+if($b==true){
   $det1 = fopen("details1.txt", "w");
   if(is_writeable("$det1")){
     fwrite($det1, $event_name);
