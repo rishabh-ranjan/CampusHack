a6fc6904b325960d76dbb31f4b56d6f48e30318f
Rishabh Ranjan <mr.rishabh.ranjan@gmail.com>
2019-02-11 00:11:57 +0530

    working mod subscribe buttons

--- mods_page_script.js
+++ mods_page_script.js
@@ -3,14 +3,38 @@
 /* global data variables */
 var mod_names = ["DevClub", "ANCC", "Music Club", "Drama Club", "Fine Arts Club"];
 var is_mod_subd = [true, false, true, false, false];
+var mod_cards = [];
 
 /* DOM variables */
 var sub_mods_wall = document.getElementById('sub_mods_wall');
 var other_mods_wall = document.getElementById('other_mods_wall');
 
 /* Functions */
+// function to toggle card position of mod_cards[i]
+function toggleModCard(ind)
+{
+	if(is_mod_subd[ind])
+	{
+		sub_mods_wall.removeChild(mod_cards[ind]);
+	}
+	else
+	{
+		other_mods_wall.removeChild(mod_cards[ind]);
+	}
+	is_mod_subd[ind] = !is_mod_subd[ind];
+	mod_cards[ind] = createModCard(ind);
+	if(is_mod_subd[ind])
+	{
+		sub_mods_wall.appendChild(mod_cards[ind]);
+	}
+	else
+	{
+		other_mods_wall.appendChild(mod_cards[ind]);
+	}
+}
+
 // returns a card object
-function createCard(ind)
+function createModCard(ind)
 {
 	let card = document.createElement('div');
 	card.classList.add('sub_mod');
@@ -23,7 +47,6 @@ function createCard(ind)
 	let image;
 	if(is_mod_subd[ind])
 	{
-		console.log('hello');
 		button.classList.add('remove_sub');
 		image = document.createElement('img');
 		image.src = 'remove.png';
@@ -35,6 +58,7 @@ function createCard(ind)
 		image.src = 'plus.png';
 	}
 	button.appendChild(image);
+	button.addEventListener('click', function(){toggleModCard(ind);}, false);
 
 	card.appendChild(title);
 	card.appendChild(button);
@@ -45,13 +69,13 @@ function createCard(ind)
 // creates and deploys all cards in the mod_names array
 for(let i = 0; i < mod_names.length; ++i)
 {
-	let card = createCard(i);
+	let card = createModCard(i);
 	if(is_mod_subd[i])
 	{
-		sub_mods_wall.appendChild(card);
+		mod_cards.push(sub_mods_wall.appendChild(card));
 	}
 	else
 	{
-		other_mods_wall.appendChild(card);
+		mod_cards.push(other_mods_wall.appendChild(card));
 	}
 }
